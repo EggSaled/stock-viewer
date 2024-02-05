e0309ed0f2eb82422d68d920a4c0ca7c0b637ec9
EggSaled <adrian.escobar.104183@gmail.com>
2024-02-04 20:26:38 -0800

    perf: separated graph code from index.js into graph.js

--- src/index.js
+++ src/index.js
@@ -1,48 +1,5 @@
-import * as d3 from 'd3';
+import renderGraph from './graph';
 import APIKEY from './secret.json';
-
-function renderGraph(data){
-  var width = 1000;
-  var height = 800;
-  var padding = 50;
-
-  var keys = Object.keys(data);
-  var meta = data[keys[0]]; // Unused for now
-  var stock = data[keys[1]];
-
-  // NOTE: The actual stock data has the date as the index, which is slightly annoying.
-  var dates = Object.keys(stock);
-  var prices = Object.values(stock);
-
-  var graph = d3.select('#output')
-  .append('svg')
-  .attr('id', 'graph')
-  .attr('width', width)
-  .attr('height', height);
-  
-  // X-axis
-  var xScale = d3.scaleTime()
-  .domain([d3.min(dates, (d) => new Date(d)), d3.max(dates, (d) => new Date(d))])
-  .range([padding, width - padding]);
-
-  var xAxis = d3.axisBottom(xScale);
-
-  graph.append('g')
-  .attr('transform', `translate(0, ${height - padding})`)
-  .call(xAxis);
-
-  // Y-axis
-  var yScale = d3.scaleLinear()
-  .domain([d3.min(prices, (d) => Number(d['4. close'])), d3.max(prices, (d) => Number(d['4. close']))])
-  .range([padding, height - padding]);
-
-  var yAxis = d3.axisLeft(yScale);
-
-  graph.append('g')
-  .attr('id', 'y-axis')
-  .attr('transform', `translate(${padding}, 0)`)
-  .call(yAxis);
-}
 
 function parseForm(event){
   event.preventDefault();
@@ -55,7 +12,10 @@ function parseForm(event){
 
   // Building API request
   // NOTE: If someone wants to run this on their own computer, they need to have their own API key.
-  var requestURL = `${APIURL}function=TIME_SERIES_${timeSeries.value.toUpperCase()}&symbol=${symbol.value}&interval=15min&apikey=${APIKEY}`;
+  // var requestURL = `${APIURL}function=TIME_SERIES_${timeSeries.value.toUpperCase()}&symbol=${symbol.value}&interval=15min&apikey=${APIKEY}`;
+
+  // NOTE: Using the demo api request for now, since 25 API requests is the daily limit.
+  var requestURL = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=demo';
 
   fetch(requestURL)
   .then((res) => res.json())
